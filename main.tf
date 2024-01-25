36ad1c77a9cf023acbebc3dc4429ca777347c1f1
IssaSacko1 <Issa.sacko@epitech.eu>
2024-01-25 14:49:06 +0100

    merging

--- main.tf
+++ main.tf
@@ -47,6 +47,14 @@ data "azurerm_resource_group" "T-cloud-3" {
   name = var.resource_group_name
 }
 
+resource "azurerm_storage_account" "sadockerpopeye901" {
+  name                     = "sadockerpopeye901"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_service_plan" "this" {
   name                = "docker-popeye-asp"
   resource_group_name = var.resource_group_name
@@ -54,7 +62,7 @@ resource "azurerm_service_plan" "this" {
   tags                = var.tags
 
   os_type  = "Linux"
-  sku_name = "F1" #! Change me
+  sku_name = "F1"
 }
 
 
@@ -69,6 +77,19 @@ resource "azurerm_linux_web_app" "docker-popeye-app-terraform" {
   site_config {
     always_on = false
   }
+
+  logs {
+    application_logs {
+        file_system_level = "Error"
+    }
+    
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 100
+      }
+    }
+ }
 }
 
 # App Service Resource currently doesn't support to set docker compose
@@ -83,4 +104,85 @@ resource "azapi_update_resource" "update_linux_web_app" {
       }
     }
   })
+}
+
+resource "azurerm_monitor_action_group" "email_action_group" {
+  name                      = "email-action-group"
+  short_name                = "exampleact"
+  resource_group_name = var.resource_group_name
+
+  email_receiver {
+    name          = "email-receiver"
+    email_address = "issa.sacko1@gmail.com"
+  }
+}
+
+# Créer une alerte pour le taux de réponse HTTP
+resource "azurerm_monitor_metric_alert" "http_response_rate_alert" {
+  name                = "http-response-rate-alert"
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_linux_web_app.docker-popeye-app-terraform.id]
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Requests"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 1000  # Exemple de seuil à ajuster
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.email_action_group.id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "http_response_time_alert" {
+  name                = "response-time-alert"
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_linux_web_app.docker-popeye-app-terraform.id]
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "AverageResponseTime"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 1000  # Exemple de seuil à ajuster
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.email_action_group.id
+  }
+}
+# Créer un espace de travail Log Analytics
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-log-analytics-workspace"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Configurer la collecte de logs pour l'application web
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name         = "example-diagnostic-settings"
+  target_resource_id = azurerm_linux_web_app.docker-popeye-app-terraform.id
+  storage_account_id = azurerm_storage_account.sadockerpopeye901.id
+
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+
+    retention_policy {
+      enabled = true
+      days    = 30
+    }
+  }
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+
+    retention_policy {
+      enabled = true
+      days    = 30
+    }
+  }
 }
