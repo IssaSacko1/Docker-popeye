3df260cbda70dcd1bad43c37ffecead463da87bd
IssaSacko1 <Issa.sacko@epitech.eu>
2024-02-08 15:18:10 +0100

    :green_heart:

--- terraform/modules/storage_account/main.tf
+++ terraform/modules/storage_account/main.tf
@@ -1,18 +1,7 @@
-resource "azurerm_storage_account" "sadockerpopeye901prod" {
+resource "azurerm_storage_account" "sadockerpopeye901dev" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_account" "sadockerpopeye901dev" {
-  resource_group_name = "T-CLO-ISA-development-environment"
-  name = "sadockerpopeye901"
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-       lifecycle {
-    ignore_changes = all
-}
 }
